f4e59865ff5cb457561822938b67803288c714c2
Viraj Mehta <viraj@tensorzero.com>
2024-03-03 21:22:15 -0500

    added writign fuction to test luma

--- test_luma.py
+++ test_luma.py
@@ -2,19 +2,20 @@ import time
 
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
+
 # from luma.core.virtual import viewport, terminal
 from luma.led_matrix.device import max7219
 from PIL import ImageFont
 
-serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
 
-font = ImageFont.truetype("pixelmix.ttf", 8)
-text = "hello"
-position = (0, 0)
-fill="white"
+class Display:
+    def __init__(self):
+        serial = spi(port=0, device=0, gpio=noop())
+        self.device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
+        self.font = ImageFont.truetype("pixelmix.ttf", 8)
+        self.position = (0, 0)
+        self.text_color = "white"
 
-with canvas(device) as draw:
-    draw.text(position, text, fill=fill, font=font)
-
-time.sleep(20)
+    def write(self, text):
+        with canvas(self.device) as draw:
+            draw.text(self.position, text, fill=self.text_color, font=self.font)
